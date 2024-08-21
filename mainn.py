e1fac19dc6ce5c39da8059e9989c2fa356a1133a
SukhoiSu35 <160200996+SukhoiSu35@users.noreply.github.com>
2024-08-21 11:10:34 +1000

    Wrk on assesmetn
    Making the GUI and coding

--- mainn.py
+++ mainn.py
@@ -2,8 +2,21 @@ from tkinter import *
 import pandas as pd
 import matplotlib.pyplot as plt
 root = Tk()
-root.title("Accident Details from 2010")
+root.title("Accident Details from 2010") #Name of the gui 
 root.configure(background = "white")
-root.maxsize(5000, 1000)
-root.minsize(500, 100)
+root.maxsize(5000, 5000)
+root.minsize(1000, 1000)
 root.geometry("500x100+10+23")
+text = Label(root,text = "Choose your data")
+text.pack()
+Dataset = pd.read_csv ('accident_details_compiled.csv')
+var = IntVar()  # variable class
+item1 = Checkbutton(root, text="Date",
+        variable=var, command=selectItems(var,"Date"))
+item1.pack(anchor='w')
+var2 = IntVar()  # variable class
+item2 = Checkbutton(root, text="Date",
+        variable=var2, command=selectItems(var2,"Time"))
+item2.pack(anchor='w')
+root.mainloop()
+
