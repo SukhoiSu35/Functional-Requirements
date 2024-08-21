766b5aa3c7373050339674dfd68560994cfafd79
SukhoiSu35 <160200996+SukhoiSu35@users.noreply.github.com>
2024-08-21 10:29:39 +1000

    GUI
    Creating the gui. Having problems running for some reason

---
+++ mainn.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import pandas as pd
+import matplotlib.pyplot as plt
+root = Tk()
+root.title("Accident Details from 2010")
+root.configure(background = "white")
+root.maxsize(5000, 1000)
+root.minsize(500, 100)
+root.geometry("500x100+10+23")
